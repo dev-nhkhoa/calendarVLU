9dd64ad663ebdb4d5036f79ada82e0f1c9fafe54
dev-nhkhoa <work.nhkhoa@gmail.com>
2024-03-26 17:04:44 +0700

    style: rename & add some uitls func

--- front-end/src/lib/utils.js
+++ front-end/src/lib/utils.js
@@ -15,4 +15,37 @@ const createHKARRAY = () => {
   return hk
 }
 
-export { createYearARRAY, createHKARRAY }
+function convert24to12(time) {
+  var hours = time.split('g')[0] // get the hours
+  var minutes = time.split('g')[1] // get the minutes
+  var part = hours >= 12 ? 'PM' : 'AM' // get AM/PM
+
+  // convert hours from 24-hour format to 12-hour format
+  hours = hours % 12
+  // 12:00 PM and 12:00 AM are special cases
+  hours = hours ? hours : 12
+
+  // return the result
+  return hours + ':' + minutes + ' ' + part
+}
+
+function addTime(time, add) {
+  if (add == '') {
+    return time
+  }
+  const aTime = time.split('g')
+  let plusHour = Math.floor(add / 60)
+  let plusMinute = add % 60
+
+  if (plusMinute + aTime[1] > 60) {
+    plusHour += 1
+    plusMinute -= 60
+  }
+
+  const newHour = parseInt(aTime[0]) + plusHour
+  const newMinute = parseInt(aTime[1]) + plusMinute
+
+  return `${newHour}g${newMinute}`
+}
+
+export { createYearARRAY, createHKARRAY, convert24to12, addTime }
