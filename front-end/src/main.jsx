921885901c135ed251cca6d914a702f68ae35d30
dev-nhkhoa <work.nhkhoa@gmail.com>
2024-03-30 13:17:00 +0700

    style: update UI

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,15 +1,41 @@
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/Home'
+import Home from './pages/HomeV2/Home'
 import { experimental_extendTheme as extendTheme } from '@mui/material'
 import { Experimental_CssVarsProvider as MuiProvider } from '@mui/material'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import '@fontsource/inter'
 import { CssBaseline } from '@mui/material'
 
+export const colors = {
+  main: '#fff',
+  bg: '#000'
+}
+
 const theme = extendTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        fontFamily: 'roboto',
+        variantMapping: {
+          large: 'h3',
+          normal: 'h2',
+          small: 'h1'
+        }
+      }
+    }
+  },
   typography: {
-    title: {
-      fontSize: 20
+    large: {
+      fontSize: '26px',
+      fontWeight: '800'
+    },
+    normal: {
+      fontSize: '20px',
+      fontWeight: '500'
+    },
+    small: {
+      fontSize: '14px',
+      fontWeight: '100'
     }
   },
   colorSchemes: {
@@ -27,8 +53,8 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
+  <MuiProvider theme={theme}>
     <CssBaseline />
-    <MuiProvider theme={theme} />
-  </RouterProvider>
+    <RouterProvider router={router} />
+  </MuiProvider>
 )
