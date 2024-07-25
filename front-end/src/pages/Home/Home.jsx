01ca03987eb7e53b06a6ec24167f4f9f5b592b4d
dev-nhkhoa <work.nhkhoa@gmail.com>
2024-07-25 22:09:50 +0700

    chore: Update Prettier configuration and package version

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -9,13 +9,14 @@ import Cookies from 'js-cookie'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { generateRandomUserID, saveUserIDToCookie } from '~/lib/handleThings'
 import Contact from '../Contact/Contact'
+import { link } from '~/user-config.json'
+import axios from 'axios'
 
 const Home = () => {
+  const [loading, setLoading] = React.useState(true)
   const [userId, setUserId] = React.useState(Cookies.get('userID'))
   const [isLogin2Vlu, setIsLogin2Vlu] = React.useState(false)
-  const [lichType, setLichType] = React.useState(
-    localStorage.getItem('lichType') || 'DrawingStudentSchedule_Perior'
-  )
+  const [lichType, setLichType] = React.useState(localStorage.getItem('lichType') || 'DrawingStudentSchedule_Perior')
 
   React.useEffect(() => {
     if (Cookies.get('userID') == undefined) {
@@ -25,19 +26,23 @@ const Home = () => {
     }
     const getUserId = Cookies.get('userID')
     setUserId(getUserId)
+
+    async function load() {
+      try {
+        const req = await axios.get(link.server.api)
+
+        if (req.status === 200) setLoading(false)
+      } catch (error) {
+        alert('Không thể kết nối với server')
+      }
+    }
+    load()
   }, [])
 
   const router = createBrowserRouter([
     {
       path: '/vlu/login',
-      element: (
-        <Login
-          userId={userId}
-          setIsLogin2Vlu={setIsLogin2Vlu}
-          lichType={lichType}
-          setLichType={setLichType}
-        />
-      )
+      element: <Login userId={userId} setIsLogin2Vlu={setIsLogin2Vlu} lichType={lichType} setLichType={setLichType} />
     },
     {
       id: 'root',
@@ -46,20 +51,14 @@ const Home = () => {
     },
     {
       path: '/vlu/calendar',
-      element: (
-        <Calendar
-          isLogin2Vlu={isLogin2Vlu}
-          userId={userId}
-          lichType={lichType}
-        />
-      )
+      element: <Calendar isLogin2Vlu={isLogin2Vlu} userId={userId} lichType={lichType} />
     },
     {
       path: '/contact',
       element: <Contact />
     }
   ])
-  return <RouterProvider router={router} />
+  return loading ? <div>Đang kết nối với SERVER...</div> : <RouterProvider router={router} />
 }
 
 export default Home
